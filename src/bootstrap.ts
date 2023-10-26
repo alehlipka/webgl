3f0d8330a62df668a209ef435332601d1049ca3a
Tolik <alehlipka@gmail.com>
2023-10-26 16:07:24 +0300

    Objects texture and position fix

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,12 +1,14 @@
 import "./assets/styles/main.scss";
 
 import {ProgramInfo, shaderCodes} from "./core/types.ts";
+import {Vector2} from "./core/math/Vector2.ts";
 import {Vector3} from "./core/math/Vector3.ts";
 
 import {Context} from "./core/Context.ts";
 import {Renderer} from "./core/Renderer.ts";
 import {Shader} from "./core/Shader.ts";
 import {Cube} from "./objects/Cube.ts";
+import {Plane} from "./objects/Plane.ts";
 
 import vertexShaderCode from './assets/shaders/vertex.glsl';
 import fragmentShaderCode from './assets/shaders/fragment.glsl';
@@ -26,7 +28,7 @@ renderer
     .addObjects([
         new Cube(gl, new Vector3(-1, +1, 0), textureTest,  Vector3.One()),
         new Cube(gl, new Vector3(+1, +1, 0), textureTest, Vector3.One()),
-        new Cube(gl, new Vector3(+0, +0, 0), textureDebug, Vector3.One()),
+        new Plane(gl, new Vector3(0, +0, 0), textureDebug, new Vector2(2,2)),
         new Cube(gl, new Vector3(+1, -1, 0), textureTest, Vector3.One()),
         new Cube(gl, new Vector3(-1, -1, 0), textureTest, Vector3.One()),
     ])
